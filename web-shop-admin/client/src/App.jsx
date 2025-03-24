37a52c23a3ed34961a11f9980c48436201691e8e
Joaip Volarevic <jvolarevic1@gmail.com>
2025-03-24 13:40:30 +0100

    -cp

---
+++ web-shop-admin/client/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './pages/login/Login'
+import Products from './pages/products/Products'
+import Layout from './components/layout/layout'
+function App() {
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path={"/login"} element={<Login />} />
+          <Route path={"/"} element={<Layout />}>
+            <Route path={"products"} element={<Products />} />
+          </Route>
+        </Routes>
+      </Router>
+    </>
+  )
+}
+export default App
