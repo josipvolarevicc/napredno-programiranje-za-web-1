a39feb723a6eb6777a0212431d53cca2940eb342
Joaip Volarevic <jvolarevic1@gmail.com>
2025-04-02 11:30:13 +0200

    -cp

--- web-shop-admin/client/src/pages/products/Products.jsx
+++ web-shop-admin/client/src/pages/products/Products.jsx
@@ -1,18 +1,29 @@
-import React, { useState } from 'react'
+import React from 'react'
 import useFetch from '../../hooks/useFetch'
 import ProductsList from '../../components/productsList/productsList'
+import styles from './Products.module.css'
+import { useState } from 'react'
+
 
 const Products = () => {
 
-  const [url, setUrl] = useState('http://localhost:3000/products')
-  const { data, error, isPending } = useFetch(url)
+  const [searchTerm, setSearchTerm] = useState('')
+  const { data, error, isPending } = useFetch(`http://localhost:3000/products?product_name`)
 
+  function handleSearchClick() {
+    setSearchTerm
+  }
   return (
     <div>
+      <div className={styles.search_div}>
+        <input type="text" className={styles.search_products_input} value={searchTerm} onChange={(e) => {setSearchTerm(e.target.value)}}/>
+        <button className={styles.search_btn} onClick={handleSearchClick}>
+          <img className={styles.search_img} src="/public/images/search-icon.png" alt="" />
+        </button>
+      </div>
       {isPending && <p>Loading...</p>}
       {error && <p>error</p>}
-      {data && <ProductsList products={data} />}
-      <button onClick={()=>setUrl('http://localhost:3000/products')}>Update search</button>
+      {data && <ProductsList products={data}/>}
     </div>
   )
 }
