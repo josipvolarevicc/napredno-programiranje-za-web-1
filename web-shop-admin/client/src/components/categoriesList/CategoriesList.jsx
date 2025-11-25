55a18d0171241ce8d06505353a88e80d782fee54
Josip <jvolarevic1@gmail.com>
2025-11-25 11:00:03 +0100

    -cp

---
+++ web-shop-admin/client/src/components/categoriesList/CategoriesList.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import style from './CategoriesList.module.css'
+import { Link } from 'react-router-dom'
+
+const CategoriesList = ({categories}) => {
+  return (
+    <div><div className={styles.container}>
+        
+            <div className={styles.table_container}>
+                <table className={styles.custom_table}>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Edit</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {categories.map((category) => (
+                            <tr key={category.id}>
+                                <td>{category.id}</td>
+                                <td>{category.name}</td>
+                                <td><Link to={`/editCategory/${category.id}`}>Edit</Link></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div></div>
+  )
+}
+
+export default CategoriesList
--- web-shop-admin/client/src/pages/categories/Categories.jsx
+++ web-shop-admin/client/src/pages/categories/Categories.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import CategoriesList from '../../components/categoriesList/CategoriesList'
+
+const dummyCategories = [
+{id: 1, name: 'Consoles'},
+  {id: 2, name: 'Displays'}
+]
+ 
+
 
 const Categories = () => {
   return (
-    <div>Categories</div>
+    <div>
+      <Link to='/addNewCategory'>Add New Categorie</Link>
+      <CategoriesList categories={dummyCategories}/>
+    </div>
   )
 }
 
